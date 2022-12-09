489117bf0112c1c3aeef632e408815e8e36f59c9
Marco Di Bella <mdb@marcodibella.de>
2022-12-09 11:23:12 +0100

    Create setup.php

---
+++ includes/setup.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Functions to activate, initiate and deactivate the plugin.
+ *
+ * @author  Marco Di Bella
+ * @package mdb-call-to-action
+ */
+
+
+defined( 'ABSPATH' ) or exit;
+
+
+
+/**
+ * Zentrale Aktivierungsfunktion für das Plugin
+ *
+ * @since   1.0.0
+ */
+
+function mdbcta__plugin_activation()
+{
+    mdbcta__add_acf_fields();
+}
+
+register_activation_hook( __FILE__, 'mdbcta__plugin_activation' );
+
+
+
+/**
+ * Lädt das Plugin-Script
+ *
+ * @since   1.1.0
+ */
+
+function mdbcta__plugin_scripts()
+{
+    wp_enqueue_style( 
+        'mdbcta-style',
+        plugins_url( 'assets/build/css/backend.min.css', __FILE__ )
+    );
+
+    wp_enqueue_script(
+        'mdbcta-script',
+        plugins_url( 'assets/build/js/backend.js', __FILE__ ),
+        'jquery',
+        PLUGIN_VERSION,
+        true
+    );
+}
+
+add_action( 'admin_enqueue_scripts','mdbcta__plugin_scripts' );
