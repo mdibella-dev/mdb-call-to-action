b10ac2f558031b64bc8530df924ec9f0b240a971
Marco Di Bella <77158987+mdibella-dev@users.noreply.github.com>
2023-02-02 13:49:47 +0100

    Remove obsolete code.

--- plugin.php
+++ plugin.php
@@ -35,7 +35,6 @@ define( __NAMESPACE__ . '\PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
 require_once( PLUGIN_DIR . 'includes/post-types/post-type-cta.php' );
 require_once( PLUGIN_DIR . 'includes/shortcodes/shortcode-cta.php' );
-require_once( PLUGIN_DIR . 'includes/acf-fields.php' );
 require_once( PLUGIN_DIR . 'includes/api.php' );
 require_once( PLUGIN_DIR . 'includes/backend.php' );
 require_once( PLUGIN_DIR . 'includes/setup.php' );
