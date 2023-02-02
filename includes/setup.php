a9502775b1dec5088aa9ccf415e3c774d940b6dd
Marco Di Bella <77158987+mdibella-dev@users.noreply.github.com>
2023-02-02 11:56:11 +0100

    Change function order.

--- includes/setup.php
+++ includes/setup.php
@@ -16,6 +16,22 @@ defined( 'ABSPATH' ) or exit;
 
 
 /**
+ * The init function for the plugin.
+ *
+ * @since 1.0.0
+ */
+
+function plugin_init()
+{
+    // Load text domain
+    load_plugin_textdomain( 'mdb-call-to-action', false, PLUGIN_DIR . '/languages' );
+}
+
+add_action( 'init', 'mdb_call_to_action\plugin_init' );
+
+
+
+/**
  * The activation function for the plugin.
  *
  * @since 1.0.0
@@ -23,7 +39,40 @@ defined( 'ABSPATH' ) or exit;
 
 function plugin_activation()
 {
+    // Do something!
     mdbcta__add_acf_fields();
 }
 
 register_activation_hook( __FILE__, 'mdb_call_to_action\plugin_activation' );
+
+
+
+/**
+ * The deactivation function for the plugin.
+ *
+ * @since 1.0.0
+ */
+
+function plugin_deactivation()
+{
+    // Do something!
+}
+
+register_deactivation_hook( __FILE__, 'mdb_call_to_action\plugin_deactivation' );
+
+
+
+/**
+ * The uninstall function for the plugin.
+ *
+ * @since 1.0.0
+ */
+
+function plugin_uninstall()
+{
+    // Do something!
+    // Delete options!
+    // Delete custom tables!
+}
+
+register_uninstall_hook( __FILE__, 'mdb_call_to_action\plugin_uninstall' );
