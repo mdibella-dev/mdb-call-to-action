55b4250ee306a224cc564242164748a5c8301516
Marco Di Bella <mdb@marcodibella.de>
2023-08-25 13:10:15 +0200

    Remove logging

--- includes/api.php
+++ includes/api.php
@@ -44,7 +44,7 @@ function get_params( $post_id ) {
     $data   = [];
     $stored = get_post_meta( $post_id, CTA_DATA_METAKEY, true );
 
-    error_log(print_r( $stored, true));
+    //error_log(print_r( $stored, true));
 
     if( false !== $stored ) :
         $data = $stored; //json_decode( $stored, true );
