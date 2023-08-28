38d6a30d9f3ce2454ac231e6c089efeee98e25c3
Marco Di Bella <mdb@marcodibella.de>
2023-08-28 18:44:36 +0200

    Change to WP code style

--- includes/classes/index.php
+++ includes/classes/index.php
@@ -6,7 +6,7 @@ defined( 'ABSPATH' ) or exit;
 
 
 // WordPress helper classes
-require_once( 'wordpress-helper/class-shortcode.php' );
+require_once 'wordpress-helper/class-shortcode.php';
 
 // Shortcode classes
-require_once( 'shortcodes/class-shortcode-cta.php' );
+require_once 'shortcodes/class-shortcode-cta.php';
