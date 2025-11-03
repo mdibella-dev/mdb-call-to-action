ad414d235d634234b7b19684e3c0670df1ed92ef
Marco Di Bella <m_dibella@icloud.com>
2025-11-03 22:05:43 +0100

    Remove helper classes

--- includes/classes/index.php
+++ includes/classes/index.php
@@ -4,12 +4,5 @@
 defined( 'ABSPATH' ) or exit;
 
 
-
-// WordPress helper classes
-require_once 'wordpress-helper/class-shortcode.php';
-require_once 'wordpress-helper/class-admin-post-list.php';
-
-
-// Plugin related classes
 require_once 'class-shortcode-cta.php' ;
 require_once 'class-admin-post-list-cta.php' ;
