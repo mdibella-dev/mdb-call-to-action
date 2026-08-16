53bdc47f6956cbd7e5dbad0356d23fd5dc4d1e8b
Marco <77158987+mdibella-dev@users.noreply.github.com>
2026-08-16 11:54:51 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -26,9 +26,6 @@ defined( 'ABSPATH' ) or exit;
 /** Variables and definitions */
 
 define( __NAMESPACE__ . '\PLUGIN_VERSION', '2.1.5' );
-define( __NAMESPACE__ . '\PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-define( __NAMESPACE__ . '\PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-
 
 define( __NAMESPACE__ . '\CTA_DATA_METAKEY', 'cta_data' );
 
